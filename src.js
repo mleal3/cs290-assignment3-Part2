38f10d0aee070519076c851d73dc89280357a5fe
Mitchel Leal <lealm@onid.oregonstate.edu>
2015-04-26 19:31:07 -0500

    Added src.js

---
+++ src.js
@@ -0,0 +1,27 @@
+function inputSave()
+{
+}
+
+function languageChoice()
+{
+	if(document.getElementById("Python").checked)
+	{
+		language.push("Python");
+	}
+	if(document.getElementById("JSON").checked)
+	{
+		language.push("JSON");
+	}
+	if(document.getElementById("JavaScript").checked)
+	{
+		language.push("JavaScript");
+	}
+	if(document.getElementById("SQL").checked)
+	{
+		language.push("SQL");
+	}
+}
+
+function getPages()
+{	
+}
